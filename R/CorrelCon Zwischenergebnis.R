75224a7b77208c783688a89c3648dcb3ecf8236f
andreas neumann <68635246+anneumann1@users.noreply.github.com>
2021-11-09 20:17:19 +0100

    Update CorrelCon Zwischenergebnis.R

--- R/CorrelCon Zwischenergebnis.R
+++ R/CorrelCon Zwischenergebnis.R
@@ -50,7 +50,7 @@ hh<-
  #       panel.border = element_rect(colour = "grey85", fill=NA, size=8),
         legend.position = "bottom",
         text=element_text(family="Times New Roman", face="bold", size=21),
-        plot.subtitle=element_text(hjust=0.5, face="italic", color="white",size=15),
+        plot.subtitle=element_text(hjust=0.5, face="italic", color="white",size=12),
         plot.title = element_text(color="grey85",hjust=.5, face="italic",size=20))
 
 ##Projections
@@ -100,7 +100,7 @@ hg<-
         legend.title = element_text(color = "white"),
         legend.position = "none",
         text=element_text(family="Times New Roman", face="bold", size=21),
-        plot.subtitle=element_text(size=15, hjust=0.5, face="italic", color="white"))+
+        plot.subtitle=element_text(size=12, hjust=0.5, face="italic", color="white"))+
   coord_flip()
 
 ##Put everything together
